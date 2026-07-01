313d55ccfa89b9441737ecb09efcc9c1313b1d03
mahendra0011 <mahendrapra0077@gmail.com>
2026-07-01 19:14:24 +0530

    attached department records to hospital context

--- server/models/Department.js
+++ server/models/Department.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const departmentSchema = new mongoose.Schema({
+  hospitalId: { type: mongoose.Schema.Types.ObjectId, ref: 'Hospital', index: true },
   name: { type: String, required: true },
   description: { type: String, default: '' },
   head: { type: String, default: '' },
