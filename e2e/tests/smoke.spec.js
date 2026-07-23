9fa8d1ac7e5d7870797098fe96a2b49907fbb79b
mahendra0011 <mahendra0011@users.noreply.github.com>
2026-07-23 03:19:28 +0000

    fix: remove silent catch swallowing e2e smoke test assertion

--- e2e/tests/smoke.spec.js
+++ e2e/tests/smoke.spec.js
@@ -5,6 +5,5 @@ test('has title', async ({ page }) => {
   await page.goto('http://localhost:8080');
   
   // Basic smoke test - just check if the page loads and has a title
-  // We'll just expect it not to crash and maybe have some text
-  await expect(page).toHaveTitle(/MediCore/i).catch(() => {});
+  await expect(page).toHaveTitle(/MediCore/i);
 });
